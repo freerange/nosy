9e861fb27c2c558a47bf939ef6df2df691610ef8
James Adam <james@lazyatom.com>
2013-01-09 13:07:21 -0600

    Basic Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nosy::Application.config.secret_token = '5cb57796bb2b8c9eb368f7dad8df4178c75cb67d69b0e369e123449ebba8f34e03b94de61851f4ef5fce0b90cd99f92d96e958020bf830796960a4c5ca6e4251'
