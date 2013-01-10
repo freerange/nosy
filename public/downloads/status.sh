a34ab4c966689b052fb89d26a2b45bc719eebc69
James Adam <james@lazyatom.com>
2013-01-10 12:44:27 -0600

    Don't output anything from the shell command.

--- public/downloads/status.sh
+++ public/downloads/status.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 URI=`ruby -ryaml -e "puts YAML.load_file(File.expand_path('~/.freerange.yml'))['GFR_STATUS_URI']"`
-curl -d"status[text]=$1" $URI/statuses
+curl -s -d"status[text]=$1" $URI/statuses -o /dev/null
